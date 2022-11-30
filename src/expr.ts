6cb8c32647f58fec46d196c59afb0ddb513f8c4e
Israel Waldner <60708054+mordy-python@users.noreply.github.com>
2022-11-30 19:20:55 +0000

    Added expr classes

---
+++ src/expr.ts
@@ -0,0 +1,205 @@
+// deno-lint-ignore-file no-unused-vars
+import { Token } from "./token.ts";
+
+abstract class ExprVisitor {
+  visit_assign_expr(expr: Assign) {
+    throw "Not Implemented";
+  }
+  visit_binary_expr(expr: Binary) {
+    throw "Not Implemented";
+  }
+  visit_call_expr(expr: Call) {
+    throw "Not Implemented";
+  }
+  visit_get_expr(expr: Get) {
+    throw "Not Implemented";
+  }
+  visit_grouping_expr(expr: Grouping) {
+    throw "Not Implemented";
+  }
+  visit_literal_expr(expr: Literal) {
+    throw "Not Implemented";
+  }
+  visit_logical_expr(expr: Logical) {
+    throw "Not Implemented";
+  }
+  visit_set_expr(expr: Set) {
+    throw "Not Implemented";
+  }
+  visit_super_expr(expr: Super) {
+    throw "Not Implemented";
+  }
+  visit_this_expr(expr: This) {
+    throw "Not Implemented";
+  }
+  visit_unary_expr(expr: Unary) {
+    throw "Not Implemented";
+  }
+  visit_variable_expr(expr: Variable) {
+    throw "Not Implemented";
+  }
+}
+
+abstract class Expr {
+  accept(_visitor: ExprVisitor) {
+    throw "Not Implemented";
+  }
+}
+
+class Assign extends Expr {
+  name: Token;
+  value: Expr;
+  constructor(name: Token, value: Expr) {
+    super();
+    this.name = name;
+    this.value = value;
+  }
+  accept(visitor: ExprVisitor) {
+    return visitor.visit_assign_expr(this);
+  }
+}
+
+class Binary extends Expr {
+  op: Token;
+  left: Expr;
+  right: Expr;
+  constructor(left: Expr, op: Token, right: Expr) {
+    super();
+    this.left = left;
+    this.op = op;
+    this.right = right;
+  }
+  accept(visitor: ExprVisitor) {
+    return visitor.visit_binary_expr(this);
+  }
+}
+
+class Call extends Expr {
+  callee: Expr;
+  paren: Token;
+  args: Array<Token>;
+  constructor(callee: Expr, paren: Token, args: Array<Token>) {
+    super();
+    this.callee = callee;
+    this.paren = paren;
+    this.args = args;
+  }
+  accept(visitor: ExprVisitor) {
+    return visitor.visit_call_expr(this);
+  }
+}
+
+class Get extends Expr {
+  obj: Expr;
+  name: Token;
+  constructor(obj: Expr, name: Token) {
+    super();
+    this.obj = obj;
+    this.name = name;
+  }
+  accept(visitor: ExprVisitor) {
+    return visitor.visit_get_expr(this);
+  }
+}
+
+class Grouping extends Expr {
+  expression: Expr;
+  constructor(expression: Expr) {
+    super();
+    this.expression = expression;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_grouping_expr(this);
+  }
+}
+
+class Literal extends Expr {
+  // deno-lint-ignore no-explicit-any
+  value: any;
+  // deno-lint-ignore no-explicit-any
+  constructor(value: any) {
+    super();
+    this.value = value;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_literal_expr(this);
+  }
+}
+
+class Logical extends Expr {
+  left: Expr;
+  op: Token;
+  right: Expr;
+  constructor(left: Expr, op: Token, right: Expr) {
+    super();
+    this.left = left;
+    this.op = op;
+    this.right = right;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_logical_expr(this);
+  }
+}
+
+class Set extends Expr {
+  obj: Expr;
+  name: Token;
+  value: Expr;
+  constructor(obj: Expr, name: Token, value: Expr) {
+    super();
+    this.obj = obj;
+    this.name = name;
+    this.value = value;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_set_expr(this);
+  }
+}
+
+class Super extends Expr {
+  keyword: Token;
+  method: Token;
+  constructor(keyword: Token, method: Token) {
+    super();
+    this.keyword = keyword;
+    this.method = method;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_super_expr(this);
+  }
+}
+
+class This extends Expr {
+  keyword: Token;
+  constructor(keyword: Token) {
+    super();
+    this.keyword = keyword;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_this_expr(this);
+  }
+}
+
+class Unary extends Expr {
+  operator: Token;
+  right: Expr;
+  constructor(operator: Token, right: Expr) {
+    super();
+    this.operator = operator;
+    this.right = right;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_unary_expr(this);
+  }
+}
+
+class Variable extends Expr {
+  name: Token;
+  constructor(name: Token) {
+    super();
+    this.name = name;
+  }
+  accept(_visitor: ExprVisitor) {
+    return _visitor.visit_variable_expr(this);
+  }
+}
